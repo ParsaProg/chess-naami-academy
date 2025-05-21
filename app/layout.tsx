706c83e77d562166ec832dd19203f75b0cd22266
ParsaProg <parsashaabani3@gmail.com>
2025-05-21 12:41:32 +0330

    fix the bugs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import SEO from "../components/SEO";
 import NavBar from "@/components/ui/NavBar";
 import Footer from "@/components/ui/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "آکادمی شطرنج نعامی",
