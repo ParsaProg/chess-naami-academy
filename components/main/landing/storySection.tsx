3c4dfce07da20710b6d2e20b8476561b85af6b57
ParsaProg <parsashaabani3@gmail.com>
2025-05-22 02:53:20 +0330

    fix the build errors

--- components/main/landing/storySection.tsx
+++ components/main/landing/storySection.tsx
@@ -1,10 +1,3 @@
 export default function StorySection(){
-    const data = [
-        {
-            image: "",
-            title: "مشاوره ثبت نام",
-            subTitle: "ثبت نام"
-        }
-    ]
     return <div className="overflow-scroll w-full"></div>
 }
