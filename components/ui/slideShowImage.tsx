273c7d088f5d63a68b066ed92f247ec9a135d42e
ParsaProg <parsashaabani3@gmail.com>
2025-06-03 01:22:20 +0330

    add the 3 slide show images

--- components/ui/slideShowImage.tsx
+++ components/ui/slideShowImage.tsx
@@ -2,15 +2,18 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import NaamiImage1 from "../../public/assets/images/naami/naami-1.jpg";
+import NaamiImage2 from "../../public/assets/images/naami/naami-2.jpg";
+import NaamiImage3 from "../../public/assets/images/naami/naami-3.jpg";
 
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 
 const images = [
-  "https://shatranjiran.com/wp-content/uploads/2023/01/01454620210906_11380320210703_%D8%B4%D8%B7%D8%B1%D9%86%D8%AC-%D8%A8%D8%B1%D8%A7%DB%8C-%DA%A9%D9%88%D8%AF%DA%A9%D8%A7%D9%86.jpg",
-  "https://elie.ir/wp-content/uploads/2022/06/children-play-chess2-min.png",
-  "https://img9.irna.ir/old/Image/1397/13970607/83016615/N83016615-72518246.jpg",
+  NaamiImage1.src,
+  NaamiImage2.src,
+  NaamiImage3.src
   // add your images here
 ];
 
@@ -24,8 +27,8 @@ export default function SwiperSlideShow() {
       // For mobile devices
       if (window.innerWidth < 550) {
         setSlidesPerView(1);
-        setSwiperHeight("180px");
-        setImageHeight("180px");
+        setSwiperHeight("400px");
+        setImageHeight("400px");
       }
       // For tablets
       else if (window.innerWidth >= 640 && window.innerWidth < 1024) {
@@ -36,8 +39,8 @@ export default function SwiperSlideShow() {
       // For laptops and larger screens
       else {
         setSlidesPerView(1);
-        setSwiperHeight("500px");
-        setImageHeight("500px");
+        setSwiperHeight("700px");
+        setImageHeight("700px");
       }
     };
 
@@ -72,12 +75,11 @@ export default function SwiperSlideShow() {
         {images.map((src, idx) => (
           <SwiperSlide key={idx}>
             <Image
-              width={900}
-              height={900}
+              width={1000}
+              height={1000}
               src={src}
               alt={`slide-${idx}`}
               style={{
-                width: "100%",
                 height: imageHeight,
                 objectPosition: "center",
                 objectFit: "cover",
