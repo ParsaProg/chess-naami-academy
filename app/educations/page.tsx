318fb4209e34199c7076164ae66e72b3f3aefbd9
ParsaProg <parsashaabani3@gmail.com>
2025-07-14 17:01:23 +0330

    the tab index ui changer created

--- app/educations/page.tsx
+++ app/educations/page.tsx
@@ -1,3 +1,49 @@
-export default function EducationPage(){
-    return <div className="flex flex-col w-[90%]"></div>
+"use client";
+
+import { RiVideoOnAiLine } from "react-icons/ri";
+import { IoBookOutline } from "react-icons/io5";
+import { FcPuzzle } from "react-icons/fc";
+import { useState } from "react";
+
+export default function EducationPage() {
+  const [selectedTapIndex, setSelectedTapIndex] = useState<number>(0);
+  return (
+    <div className="flex flex-col w-[90%] mt-[50px] mx-auto">
+      <div className="w-full flex shadow-lg bg-slate-100 items-center justify-between rounded-lg p-2 font-bold text-black text-xl gap-x-5 text-center">
+        <div
+          onClick={() => {
+            setSelectedTapIndex(0);
+          }}
+          className={`transition-all duration-200 ${
+            selectedTapIndex === 0 ? "bg-slate-300" : "bg-transparent"
+          } w-full rounded-md flex items-center gap-x-2 p-3 justify-center cursor-pointer`}
+        >
+          <RiVideoOnAiLine size={20} />
+          ویدیو‌های آموزشی
+        </div>
+        <div
+          onClick={() => {
+            setSelectedTapIndex(1);
+          }}
+          className={`transition-all duration-200 w-full ${
+            selectedTapIndex === 1 ? "bg-slate-300" : "bg-transparent"
+          } rounded-md flex items-center gap-x-2 p-3 justify-center cursor-pointer`}
+        >
+          <IoBookOutline size={20} />
+          کتاب‌های رایگان
+        </div>
+        <div
+          onClick={() => {
+            setSelectedTapIndex(2);
+          }}
+          className={`transition-all duration-200 w-full ${
+            selectedTapIndex === 2 ? "bg-slate-300" : "bg-transparent"
+          } rounded-md flex items-center gap-x-2 p-3 justify-center cursor-pointer`}
+        >
+          <FcPuzzle size={20} />
+          پازل‌ها
+        </div>
+      </div>
+    </div>
+  );
 }
