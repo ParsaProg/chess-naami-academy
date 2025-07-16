ec50ff0fac36ebd8ca47e2073d06e55340a7438d
ParsaProg <parsashaabani3@gmail.com>
2025-07-17 02:10:18 +0330

    Article schema for mongo data base uploaded and updated

--- models/Article.ts
+++ models/Article.ts
@@ -1,24 +1,21 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const ArticleSchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true },
-    content: { type: String, required: true },
-    cats: {type: Array, required: true},
-    titleImage: {type:File, required: true},
-    views: {type:String, required: true},
-    likes: {type:String, required: true},
-    importantText: {type:String, required: true},
-    desc: {type:String, required: true},
-    time: {type:String, required: true},
-    publishDate: {type:String, required: true},
-    publisherName: {type:File, required: true},
-    publisherImage: {type:String, required: true},
-    publisherTag: {type:String, required: true},
-    comments: {type:String, required: true},
-    isSpecial: {type: Boolean, required: true},
-  },
-  { timestamps: true }
-)
+const ArticleSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  cats: { type: [String], required: true },
+  titleImage: { type: String, required: true },
+  views: { type: String, default: "0" },
+  likes: { type: String, default: "0" },
+  importantText: { type: String, required: true },
+  desc: { type: String, required: true },
+  time: { type: String, required: true },
+  publishDate: { type: String, required: true },
+  publisherName: { type: String, required: true },
+  publisherImage: { type: String, required: true },
+  publisherTag: { type: String, required: true },
+  comments: { type: String, default: "0" },
+  isSpecial: { type: Boolean, default: false }
+}, { timestamps: true });
 
-export default mongoose.models.Article || mongoose.model('Article', ArticleSchema)
+export default mongoose.models.Article || mongoose.model('Article', ArticleSchema);
