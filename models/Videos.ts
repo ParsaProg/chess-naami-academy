436447dd79ba25d05421a626689dabddf8ad5178
ParsaProg <parsashaabani3@gmail.com>
2025-07-24 09:17:25 +0330

    Videos model or schema created

--- models/Videos.ts
+++ models/Videos.ts
@@ -1,5 +1,13 @@
 import mongoose from "mongoose";
 
 const VideoSchema = new mongoose.Schema({
+    posterImage: {type: String, required: true},
+    level: {type: String, required: true},
+    time: {type: String, required: true},
+    title: {type: String, required: true},
+    views: {type: String, required: true},
+    publisher: {type: String, required: true},
+    videoLink: {type: String, required: true}, 
+}, { timestamps: true });
 
-}, {timestamps: true})
+export default mongoose.models.Videos || mongoose.model("Videos", VideoSchema);
