8cbb8fac787ded96af2468ff8d0df29fd40b76e3
ParsaProg <parsashaabani3@gmail.com>
2025-07-29 22:37:49 +0330

    fix build errors with add videos data to a section of videos

--- app/educations/widgets/Videos.tsx
+++ app/educations/widgets/Videos.tsx
@@ -19,6 +19,7 @@ export default function Videos() {
         });
         const videoData = await response.json();
         setVideosData(videoData);
+        console.log(videosData)
       } catch (error: unknown) {
         console.log(error);
       }
