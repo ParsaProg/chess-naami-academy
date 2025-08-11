bb42a5595ed69f7b2d01d5ae7890d5a45de1909b
ParsaProg <parsashaabani3@gmail.com>
2025-08-11 12:32:07 +0330

    book schema added

---
+++ models/Books.ts
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const BookSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    subTitle: { type: String, required: true },
+    author: { type: String, required: true },
+    pages: { type: Number, required: true },
+    size: { type: String, required: true },
+    level: { type: String, required: true },
+    downlaods: { type: String, required: true },
+    pdfLink: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.models.Books || mongoose.model("Book", BookSchema);
