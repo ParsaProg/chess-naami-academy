1685c189d9084cd4a9cc85da10ccb10914488b69
ParsaProg <parsashaabani3@gmail.com>
2025-08-16 14:43:36 +0330

    security env files key updated: page.tsx

--- app/educations/page.tsx
+++ app/educations/page.tsx
@@ -1,20 +1,47 @@
 "use client";
-import { useState } from "react";
+
+import { useEffect, useState } from "react";
 import TabSelect from "./widgets/TabSelect";
 import Videos from "./widgets/Videos";
 import Books from "./widgets/Books";
 import Puzzles from "./widgets/Puzzles";
 import { AnimatePresence } from "framer-motion";
 import { motion } from "framer-motion";
-
+import { usePathname, useSearchParams } from "next/navigation";
 export default function EducationPage() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
   const [selectedTapIndex, setSelectedTapIndex] = useState<number>(0);
+
+  const updateSearchParams = (param: string, value: string) => {
+    const params = new URLSearchParams(searchParams!);
+    params.set(param, value);
+    
+    // Update URL without refresh
+    window.history.pushState({}, "", `${pathname}?${params.toString()}`);
+    
+    // Optional: Manually trigger a state update if needed
+    // (e.g., if components depend on search params)
+    window.dispatchEvent(new Event("popstate"));
+  };
+
+  useEffect(() => {
+    const tabParam = searchParams?.get("tab");
+    if (tabParam === undefined || tabParam === null || tabParam === "videos") {
+      setSelectedTapIndex(0);
+    } else if (tabParam === "books") {
+      setSelectedTapIndex(1);
+    } else if (tabParam === "puzzles") {
+      setSelectedTapIndex(2);
+    }
+  }, [selectedTapIndex]);
 
   return (
     <div className="flex flex-col w-[90%] mt-[50px] mx-auto">
       <TabSelect
         selectedTapIndex={selectedTapIndex}
         setSelectedTapIndex={setSelectedTapIndex}
+        updateSearchParams={updateSearchParams}
       />
       <AnimatePresence mode="wait">
         {selectedTapIndex === 0 && (
