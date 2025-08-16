a32e6fb50004a08a7e4907fe0ea651bc5a7d519c
ParsaProg <parsashaabani3@gmail.com>
2025-08-16 15:14:51 +0330

    add suspense for education page

--- app/educations/page.tsx
+++ app/educations/page.tsx
@@ -1,94 +1,11 @@
-"use client";
+// app/educations/page.tsx
+import { Suspense } from 'react';
+import EducationsPage from './educationWidget';
 
-import { useEffect, useState } from "react";
-import TabSelect from "./widgets/TabSelect";
-import Videos from "./widgets/Videos";
-import Books from "./widgets/Books";
-import Puzzles from "./widgets/Puzzles";
-import { AnimatePresence } from "framer-motion";
-import { motion } from "framer-motion";
-import { usePathname, useSearchParams } from "next/navigation";
-export default function EducationPage() {
-  const pathname = usePathname();
-  const searchParams = useSearchParams();
-  const [selectedTapIndex, setSelectedTapIndex] = useState<number>(0);
-
-  const updateSearchParams = (param: string, value: string) => {
-    const params = new URLSearchParams(searchParams!);
-    params.set(param, value);
-    
-    // Update URL without refresh
-    window.history.pushState({}, "", `${pathname}?${params.toString()}`);
-    
-    // Optional: Manually trigger a state update if needed
-    // (e.g., if components depend on search params)
-    window.dispatchEvent(new Event("popstate"));
-  };
-
-  useEffect(() => {
-    const tabParam = searchParams?.get("tab");
-    if (tabParam === undefined || tabParam === null || tabParam === "videos") {
-      setSelectedTapIndex(0);
-    } else if (tabParam === "books") {
-      setSelectedTapIndex(1);
-    } else if (tabParam === "puzzles") {
-      setSelectedTapIndex(2);
-    }
-  }, [selectedTapIndex]);
-
+export default function Educations() {
   return (
-    <div className="flex flex-col w-[90%] mt-[50px] mx-auto">
-      <TabSelect
-        selectedTapIndex={selectedTapIndex}
-        setSelectedTapIndex={setSelectedTapIndex}
-        updateSearchParams={updateSearchParams}
-      />
-      <AnimatePresence mode="wait">
-        {selectedTapIndex === 0 && (
-          <motion.div
-            key="video"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 0.2, ease: "easeIn" },
-            }}
-            transition={{ duration: 0.2, ease: "easeIn" }}
-          >
-            <Videos />
-          </motion.div>
-        )}
-        {selectedTapIndex === 1 && (
-          <motion.div
-            key="books"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{
-              opacity: 0,
-
-              transition: { duration: 0.2, ease: "easeIn" },
-            }}
-            transition={{ duration: 0.2, ease: "easeIn" }}
-          >
-            <Books />
-          </motion.div>
-        )}
-        {selectedTapIndex === 2 && (
-          <motion.div
-            key="puzzles"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{
-              opacity: 0,
-
-              transition: { duration: 0.2, ease: "easeIn" },
-            }}
-            transition={{ duration: 0.2, ease: "easeIn" }}
-          >
-            <Puzzles />
-          </motion.div>
-        )}
-      </AnimatePresence>
-    </div>
+    <Suspense fallback={<div>در حال بارگذاری...</div>}>
+      <EducationsPage />
+    </Suspense>
   );
 }
