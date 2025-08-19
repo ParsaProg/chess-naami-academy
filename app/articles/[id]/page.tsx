708fdf65d8d627951c2be452664c6dafa53a9fca
ParsaProg <parsashaabani3@gmail.com>
2025-08-19 16:13:47 +0330

    create article slug social media tags

--- app/articles/[id]/page.tsx
+++ app/articles/[id]/page.tsx
@@ -1,9 +1,5 @@
-"use client";
-
-import { useEffect, useState } from "react";
-import SpecialArticleContainer from "@/components/ui/articles/special-articles-container";
-import "../../../styles/loaderSpinner.css";
-import Image from "next/image";
+import { Metadata } from "next";
+import ArticleClient from "./ArticleClient";
 
 interface Article {
   _id: string;
@@ -25,98 +21,55 @@ interface Article {
   isSpecial: boolean;
 }
 
-export default function MainArticlesDetailsPage() {
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-  const [article, setArticle] = useState<Article>();
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata> {
+  const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || "https://chessnaami.ir";
 
-  useEffect(() => {
-    const pathParts = window.location.pathname.split("/");
-    const id = decodeURIComponent(pathParts[pathParts.length - 1]);
+  const res = await fetch(`${baseUrl}/admin/api/articles/${params.slug}`, {
+    headers: {
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_SECRET_TOKEN}`,
+    },
+    cache: "no-store",
+  });
 
-    const fetchData = async () => {
-      try {
-        setIsLoading(true);
-        const response = await fetch("/admin/api/articles", {
-          headers: {
-            Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_SECRET_TOKEN}`,
-          },
-        });
-        if (!response.ok) throw new Error("Unauthorized or server error");
-
-        const data = await response.json();
-        const mainArticle = data.find(
-          (article: Article) => article.title === id
-        );
-        setArticle(mainArticle);
-      } catch (error) {
-        console.error("Error fetching article data:", error);
-      } finally {
-        setIsLoading(false);
-      }
+  if (!res.ok) {
+    return {
+      title: "مقاله یافت نشد",
+      description: "خطا در بارگذاری مقاله",
     };
-
-    fetchData();
-  }, []);
-
-  if (isLoading) {
-    return (
-      <div className="flex items-center justify-center w-full mx-auto">
-        <span className="loader mt-[50px] mx-auto"></span>
-      </div>
-    );
   }
 
-  if (!article) {
-    return (
-      <h1 className="text-black mt-[50px] w-full text-center font-bold text-3xl">
-        مقاله‌ای با این نام یافت نشد
-      </h1>
-    );
-  }
+  const article: Article = await res.json();
 
-  return (
-    <div className="flex flex-col items-start w-[90%] mx-auto">
-      <div className="mt-10 w-full">
-        <SpecialArticleContainer
-          imageTitle={article.titleImage}
-          title={article.title}
-          cats={article.cats}
-          desc={article.desc}
-          publishDate={article.publishDate}
-          time={article.time}
-          views={article.views}
-          likes={article.likes}
-          isSpecial={article.isSpecial}
-        />
-      </div>
+  return {
+    title: article.title,
+    description: article.desc,
+    openGraph: {
+      title: article.title,
+      description: article.desc,
+      images: [
+        {
+          url: `${article.titleImage}`,
+          width: 1200,
+          height: 630,
+          alt: article.title,
+        },
+      ],
+      type: "article",
+      url: `${baseUrl}/articles/${params.slug}`,
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: article.title,
+      description: article.desc,
+      images: [`${baseUrl}${article.titleImage}`],
+    },
+  };
+}
 
-      <div className="flex flex-col sm:flex-row items-center justify-between mt-8 w-full rounded-lg shadow-xl border border-slate-300 p-5 gap-5 sm:gap-0">
-        <section className="flex flex-row items-center gap-x-2">
-          <div
-            className="rounded-full border border-slate-400 w-14 h-14 overflow-hidden"
-          >
-            <Image alt="شطرنج ایران, مقالات شطرنج ایران" src={article.publisherImage} unoptimized width={800} height={800} className="w-14 h-14"/>
-          </div>
-          <div className="flex flex-col">
-            <span className="text-lg font-semibold text-slate-900">
-              {article.publisherName}
-            </span>
-            <span className="text-md text-slate-600">
-              {article.publisherTag}
-            </span>
-          </div>
-        </section>
-      </div>
-
-      <div className="items-start mt-8 w-full rounded-lg shadow-xl border border-slate-300 p-5">
-        <p className="text-justify text-lg leading-10">{article.content}</p>
-        <div className="mt-5 border-r-4 border-amber-400 rounded-lg bg-amber-50 px-8 py-12">
-          <h1 className="font-bold text-xl text-amber-950">نکته مهم:</h1>
-          <h5 className="font-normal text-amber-700 mt-3">
-            {article.importantText}
-          </h5>
-        </div>
-      </div>
-    </div>
-  );
+export default function MainArticlesDetailsPage() {
+  return <ArticleClient />;
 }
