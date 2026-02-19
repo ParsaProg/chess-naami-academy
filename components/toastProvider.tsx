10fcf3033773a835335f11fa40cf4a60a54317d1
ParsaProg <parsashaabani3@gmail.com>
2026-02-19 17:07:04 +0330

    Toast provider updates and changes

--- components/toastProvider.tsx
+++ components/toastProvider.tsx
@@ -2,21 +2,33 @@
 'use client';
 
 import { ToastContainer } from 'react-toastify';
+import { Toaster } from 'react-hot-toast';
 import 'react-toastify/dist/ReactToastify.css';
 
 export default function ToastProvider() {
   return (
-    <ToastContainer
-      position="top-center"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={true} // For Persian/RTL languages
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="light"
-    />
+    <>
+      <ToastContainer
+        className="z-[999999]"
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={true}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          duration: 4000,
+          style: { direction: 'rtl' },
+        }}
+        containerStyle={{ zIndex: 999999 }}
+      />
+    </>
   );
 }
